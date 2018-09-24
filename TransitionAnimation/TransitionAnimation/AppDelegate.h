806910b841159b5acbf1a47b08d87987ebdd0d67
hongqingWang <272338444@qq.com>
2018-09-24 17:16:43 +0800

    初始化项目
    初始化项目

---
+++ TransitionAnimation/TransitionAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TransitionAnimation
+//
+//  Created by 王红庆 on 2018/9/24.
+//  Copyright © 2018年 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TransitionAnimation/TransitionAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransitionAnimation
+//
+//  Created by 王红庆 on 2018/9/24.
+//  Copyright © 2018年 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
