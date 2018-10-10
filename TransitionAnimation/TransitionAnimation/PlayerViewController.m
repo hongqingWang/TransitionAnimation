27e0ed00973cab3b43d67a47c90aa5ebc57dbea0
hongqingWang <272338444@qq.com>
2018-10-10 23:08:40 +0800

    解除转场动画，设置小于一定角度，恢复视图

--- TransitionAnimation/TransitionAnimation/PlayerViewController.m
+++ TransitionAnimation/TransitionAnimation/PlayerViewController.m
@@ -21,7 +21,7 @@
     
     self.view.layer.anchorPoint = CGPointMake(0.5, 1.0);
     
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor blueColor];
     
     self.view.frame = [UIScreen mainScreen].bounds;
     
@@ -39,20 +39,39 @@
     CGPoint point = [recognizer translationInView:self.view];
     
     CGAffineTransform transform = self.view.transform;
-    CGFloat viewAngle = atan2(transform.b, transform.a);
     
-    CGFloat dx = point.x * cos(viewAngle);
-    CGFloat dy = point.y * sin(viewAngle);
-    
-    CGFloat angle = (dx + dy) / self.view.bounds.size.width;
-    
-    transform = CGAffineTransformRotate(transform, angle);
-    transform.tx += 1 * (dx + dy);
-    //    transform = CGAffineTransformTranslate(transform, dx + dy, 0);
-    
-    self.view.transform = transform;
-    
-    [recognizer setTranslation:CGPointZero inView:self.view];
+    if (recognizer.state == UIGestureRecognizerStateEnded || recognizer.state == UIGestureRecognizerStateCancelled) {
+        
+        if (ABS(transform.b) > 0.25) {
+            [self dismissViewControllerAnimated:YES completion:nil];
+        } else {
+            [UIView animateWithDuration:0.25
+                                  delay:0.0
+                 usingSpringWithDamping:0.8
+                  initialSpringVelocity:10
+                                options:0 animations:^{
+                                    self.view.transform = CGAffineTransformIdentity;
+                                } completion:^(BOOL finished) {
+                                    
+                                }];
+        }
+        
+    } else if (recognizer.state == UIGestureRecognizerStateChanged) {
+        
+        CGFloat viewAngle = atan2(transform.b, transform.a);
+        
+        CGFloat dx = point.x * cos(viewAngle);
+        CGFloat dy = point.y * sin(viewAngle);
+        
+        CGFloat angle = (dx + dy) / self.view.bounds.size.width;
+        
+        transform = CGAffineTransformRotate(transform, angle);
+        transform.tx += 1 * (dx + dy);
+        
+        self.view.transform = transform;
+        
+        [recognizer setTranslation:CGPointZero inView:self.view];
+    }
 }
 
 @end
